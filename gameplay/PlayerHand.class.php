9f6318536f66f830ebfa4fcd7566488a3f54709e
Josh Kramer <jdk3414@rit.edu>
2013-03-17 01:39:00 -0400

    Partly implemented PlayerHand class

--- gameplay/PlayerHand.class.php
+++ gameplay/PlayerHand.class.php
@@ -8,7 +8,53 @@
 	 **/
 	class PlayerHand{
 		
+		private $_cards = array();
+		private $
+		public function __construct(){}
+
+		public function add($playingCard){
+			if(!is_object($playingCard)){
+				throw new Exception("Tried adding a card to a PlayerHand that is not an object.");
+			}
+
+			if(get_class($playingCard) != get_class(new PlayingCard(1, "club"))){
+				throw new Exception("Tried adding a card to a PlayerHand that was not a card.");
+			}
+
+			// Confirmed that $playingCard is a card
+			$_cards[] = $playingCard;
+		}
+
+		public function remove($playingCard){
+			if(!is_object($playingCard)){
+                throw new Exception("Tried adding a card to a PlayerHand that is not an object.");
+            }
+
+            if(get_class($playingCard) != get_class(new PlayingCard(1, "club"))){
+                throw new Exception("Tried adding a card to a PlayerHand that was not a card.");
+            }
+
+			foreach($index=>$this->_cards as $card){
+				if($playingCard->equals($card)){
+					//remove
+					unset($this->_cards[$index]);
+					return $card;
+				}
+			}
+			return false;
+		}
+
+		public function totalPoints(){
+			//count points
+		}
+
+		public function numberOfCardsInHand(){
+			return count($this->_cards);
+		}
+		
+		
 
 	}
+
 
 ?>
