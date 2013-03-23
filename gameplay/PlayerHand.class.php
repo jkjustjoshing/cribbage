acc4f622de7325fc3a2dd1bc8885efb2853a84b2
Josh Kramer <jdk3414@rit.edu>
2013-03-23 16:02:35 -0400

    Run scoring not working at all.

--- gameplay/PlayerHand.class.php
+++ gameplay/PlayerHand.class.php
@@ -11,6 +11,12 @@
 	class PlayerHand{
 		
 		/**
+		 * Used to tell if this hand is a crib or not
+		 */
+		const CRIB = 1;
+		const NOT_CRIB = 0;
+
+		/**
 		 * An array of the cards stored in the hand
 		 */	
 		private $_cards = array();
@@ -29,14 +35,15 @@
 		 * Stores if this hand is a crib or not. The only difference
 		 * is how flushes are calculated
 		 */
-		private $_isCrib = false;
+		private $_isCrib;
 		
 		/**
 		 * Constructor for the hand. Can optionally pass an array of cards that will
 		 * be put into the hand.
 		 * @throws InvalidArgumentException If any items in the passed array are not card objects
 		 */
-		public function __construct($cardArray = array()){
+		public function __construct($isCrib = self::NOT_CRIB, $cardArray = array()){
+			$this->_isCrib = $isCrib;
 			try{
 				foreach($cardArray as $card){
 					if(!is_object($card)){
@@ -113,7 +120,7 @@
 		 * cut card.
 		 * @return The number of points in the hand with the given cut card
 		 */
-		public function totalPoints($cutCard){
+		public function totalPoints($cutCard, $debug = false){
 			if($this->numberOfCardsInHand() < 4){
 				// Score can't be calculated with a smaller-than-full hand
 				return 0;
@@ -134,7 +141,7 @@
 				 */
 
 				// Look for flush
-				$suit = $this->_cards[0]->getSuit();
+				$suit = reset($this->_cards)->getSuit(); // Get suit of first item (even if first index isn't predictable)
 				$flush = true;
 				foreach($this->_cards as $card){
 					if($card->getSuit() != $suit){
@@ -145,7 +152,7 @@
 				if($flush){
 					if($cutCard->getSuit() == $suit){
 						$this->_score += 5;
-					}else if(!$this->_isCrib){
+					}else if(!$this->_isCrib == self::CRIB){
 						$this->_score += 4;
 					}
 				}
@@ -159,10 +166,70 @@
 				}
 
 
-				// Sort the cards first
-				//usort($this->_score
 
-				//$this->_score = $this->recursivePointsSearch(;
+if($debug)echo "<pre>entering points search\n";	
+				$cardsPlusCut = $this->_cards;
+				$cardsPlusCut[] = $cutCard;
+
+				usort($cardsPlusCut, function($a, $b){
+                    if(!is_object($a) || !is_object($b)){
+                        throw new Exception("Comparing non-objects");
+                    }
+
+                    if($a->getNumber() == $b->getNumber()) return 0;
+                    return ($a->getNumber() < $b->getNumber() ? -1 : 1);
+                });
+                $cardsPlusCut = array_values($cardsPlusCut);
+
+				// Look for pairs and straights
+				$runLength = 0;
+                $multiplier = 1;
+                $multiplierForThisNumber = 1;
+                $thisNumberForMultiplier = 0;
+				for($i = 0; $i < count($cardsPlusCut); ++$i){
+
+					// Look for pairs
+					for($j = $i+1; $j < count($cardsPlusCut); ++$j){
+						if($cardsPlusCut[$i]->getNumber() == $cardsPlusCut[$j]->getNumber()){
+							$this->_score += 2;
+						}
+					}
+					
+					// Look for straights
+if($debug)					echo "______________\n";
+if($debug)					echo "straight search card $i -> $cardsPlusCut[$i]\n";
+
+					if($runLength == 0){
+if($debug)						echo "run length 0";
+						$runLength = 1;
+						$thisNumberForMultiplier = $cardsPlusCut[$i]->getNumber();
+					}else{
+						if($thisNumberForMultiplier == $cardsPlusCut[$i]->getNumber()){
+							$multiplierForThisNumber++;
+						}else if($thisNumberForMultiplier + 1 == $cardsPlusCut[$i]->getNumber()){
+							$runLength++;
+							$multiplier *= $multiplierForThisNumber;
+							$multiplierForThisNumber = 1;
+							$thisNumberForMultiplier++;
+						}else{
+							$multiplier *= $multiplierForThisNumber;
+							$this->_score += $runLength * $multiplier;
+							
+							$thisNumberForMultiplier = $cardsPlusCut[$i]->getNumber();
+							$multiplierForThisNumber = 1;
+							$multiplier = 1;
+							$runLength = 1;
+						}
+					}
+if($debug)				echo "multiplier - $multiplier\n";
+if($debug)				echo "multiplierForThisNumber - $multiplierForThisNumber\n";
+if($debug)				echo "runLength - $runLength\n";
+if($debug)				echo "thisNumberForMultiplier - $thisNumberForMultiplier\n";
+						
+				}
+
+				$this->_score += $this->recursivePointsSearch($cardsPlusCut, $debug);
+if($debug)		echo "</pre>";
 				return $this->_score;
 			}
 		}
@@ -172,53 +239,85 @@
 		  * points for each combination.
 		  * Assume all cards are initially sorted
 		 **/
-		private function recursivePointsSearch($currentArray = array()){
+		private function recursivePointsSearch($currentArray = array(), $debug = false){
+if($debug)			echo "called recursivePointsSearch\n";
 			if(!is_array($currentArray)){
 				throw new Exception('$currentArray must be an array');
 			}
-
-			if(count($currentArray) == 0){
-				$accumulator = 0;
-				foreach($this->_cards as $card){
-					$accumulator += recursivePointsSearch(array($card));					
-				}
-				return $accumulator;
-			}else{
-				// Get card set's points, then recursively call method
-				$pointAccumulator= 0;
-
-				// If the cards are 2, check for a pair
-				if(count($currentArray) == 2){
-					$number = -1;
-					foreach($currentArray as $pairCheckCard){
-						if($number == -1){
-							 $number = $pairCheckCard->getNumber();
-						}else if ($pairCheckCard->getNumber() == $number){
-							// Pair found!
-							$pointsAccumulator += 2;
-						}
+			
+			$pointAccumulator = 0;
+			// If we are just calling this, reset the keys to the array and order
+if($debug){//          print_r($currentArray);
+	echo "-------------------";
+	foreach($currentArray as $card){
+		echo $card->getNumber() . ", ";
+	}
+	echo "\n";
+	
+}
+			// If the cards are 2, check for a pair
+/*			if(count($currentArray) == 2){
+				$number = -1;
+				foreach($currentArray as $pairCheckCard){
+					if($number == -1){
+						 $number = $pairCheckCard->getNumber();
+					}else if($pairCheckCard->getNumber() == $number){
+						// Pair found!
+						$pointAccumulator += 2;
 					}
 				}
-
-				// Check for 15
-				$fifteenAccumulator = 0;
-				foreach($currentArray as $fifteenCheckCard){
-					$fifteenAccumulator += $fifteenCheckCard->getCountValue();
-				}
-				if($fifteenAccumulator == 15){
-					$pointsAccumulator += 2;
-				}
-
-				// Check for straight
-				
-				$indexOfLastCardInSet = max(array_keys($currentArray));
-				
 			}
-			
-			
-
-			if($indexOfLastCardInSet == count($this->_cards)){
-				//Last card is in set, stop recursing
+*/
+			// Check for 15
+			$fifteenAccumulator = 0;
+			foreach($currentArray as $fifteenCheckCard){
+				$fifteenAccumulator += $fifteenCheckCard->getCountValue();
+			}
+			if($fifteenAccumulator == 15){
+				$pointAccumulator += 2;
+			}
+if($debug) echo "points post 15 - $pointAccumulator\n";
+/*			// Check for straight
+			if(count($currentArray) >= 3){
+				$run = true;
+				$firstCard = array_slice($currentArray, 0, 1);
+				$lastNumber = $firstCard[0]->getNumber() - 1;
+				foreach($currentArray as $card){
+					if($lastNumber+1 != $card->getNumber()){
+						$run = false;
+						break;
+					}else{
+						++$lastNumber;
+					}
+				}
+				if($run){
+					if(count($currentArray) == 3){
+						$pointAccumulator += 3;
+					}else if(count($currentArray) == 4){
+						$pointAccumulator -= 2; // Already 6 points from the 2x3 card run but should only be 4 total 
+					}else if(count($currentArray) == 5){
+						//Already at 5, shouldn't change
+					}
+				}
+			}	
+*/
+if($debug) echo "points after run - $pointAccumulator\n";
+			if(count($currentArray) == 2){
+if($debug)					echo "2 left\n";
+				// End of points counting, return accumulator
+				return $pointAccumulator;
+			}else{
+if($debug)				echo "recurse...\n";
+				// Remove each possible card and recursively call
+				for($i = 0; $i < 5; ++$i){
+					if(!isset($currentArray[$i])){
+						break; // So we have no duplicate card sets
+					}
+					$tempArray = $currentArray;
+					unset($tempArray[$i]);
+					$pointAccumulator += $this->recursivePointsSearch($tempArray, $debug);
+				}
+				return $pointAccumulator;
 			}
 
 		}
