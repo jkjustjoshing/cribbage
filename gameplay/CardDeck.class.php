2ea24ed1d8d72d8685eb819a2bb719b986d814c0
Josh Kramer <jdk3414@rit.edu>
2013-03-24 22:06:16 -0400

    Implemented and partially tested CardDeck class

--- gameplay/CardDeck.class.php
+++ gameplay/CardDeck.class.php
@@ -3,8 +3,12 @@
 	require_once("PlayingCard.class.php");
 
 	class CardDeck{
-		
+
+		const SHUFFLE = true;
+				
 		private $_cards = array();
+
+		private $_cutCard;
 
 		public function __construct($shuffle = false){
 			
@@ -16,12 +20,34 @@
 			}
 
 			if($shuffle){
+				// Shuffle is a native method
 				shuffle($this->_cards);
 			}
 
 		}
 
+		public function shuffle(){
+			if(count($this->_cards) != 52){
+				throw Exception("Can't shuffle a deck that isn't full.");
+			}
+			shuffle($this->_cards);
+		}
 
+		public function pop(){
+			return array_pop($this->_cards);
+		}
+
+		public function pickCutCard($index){
+			$card = $this->_cards[$index];
+			unset($this->_cards[$index]);
+			$this->_cards = array_values($this->_cards);
+			
+			return $card;
+		}
+
+		public function size(){
+			return count($this->_cards);
+		}
 
 	}
 
