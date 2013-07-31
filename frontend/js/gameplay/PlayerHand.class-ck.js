9cc787b1ea69de2e5e5ee23b0f122fd4071ff4bd
Josh Kramer <jdk3414@rit.edu>
2013-07-31 02:41:25 -0400

    Removed User and ~ folders - shouldn't have ever been tracked. Added a few features do Draggable.class.js, and used it for dragging cards to the crib. Still not fully implemented

--- frontend/js/gameplay/PlayerHand.class-ck.js
+++
@@ -1,7 +0,0 @@
-/**
- * PlayerHand object
- * Represents a Player's hand, both in memory and on the screen in the DOM
- * 
- * @param {Boolean} isCrib
- * @param {Array}  cardArray
- */function PlayerHand(e,t){this.isCrib=e;this.cards=[];for(var n=0;n<t.length;++n)t[n]instanceof PlayerCard?this.cards[this.cards.length]=t[n]:this.cards[this.cards.length]=new PlayerCard(0,"")}PlayerHand.prototype.remove=function(e){for(var t=0;t<this.cards.length;++t)if(this.cards[t].equals(e)){this.cards.splice(t);return!0}return!1};
